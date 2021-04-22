513c513b91bbf156e4ed80787f603f03b3c7c385
Dishikagoel <64327129+Dishikagoel@users.noreply.github.com>
2021-04-22 11:21:51 -0400

    Add files via upload

---
+++ dany-python/script.py
@@ -0,0 +1,13 @@
+import quandl
+import pandas as pd 
+import numpy as np 
+import matplotlib.pyplot as plt 
+from datatime import datatime
+from pandas_datareader import data
+%matplotlib inline
+
+#setting up the api
+quandl.Apiconfig.api_key = 'Z8jUsau5pGWyAyokp73_'
+
+#getting and storing the data
+
